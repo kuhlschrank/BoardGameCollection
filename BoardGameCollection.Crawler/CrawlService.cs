17fe74a1ffc8d6c62500f7c5d2ee9f3257256ebf
Florian Dötsch <florian.doetsch@outlook.com>
2019-12-13 21:35:03 +0100

    first crawler

---
+++ BoardGameCollection.Crawler/CrawlService.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BoardGameCollection.Crawler
+{
+    public class CrawlService : IHostedService
+    {
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return Task.Run(async () =>
+            {
+                while (true)
+                {
+                    await ExecuteAsync();
+                    await Task.Delay(5000, cancellationToken);
+                }
+            });
+        }
+
+        public async Task ExecuteAsync()
+        {
+            Console.Write("Crawling...");
+            await Task.CompletedTask;
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
