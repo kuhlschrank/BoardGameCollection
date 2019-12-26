a71e798b6da1f093d8db1a871b090a56695b837d
Florian Dötsch <florian.doetsch@outlook.com>
2019-12-26 20:31:58 +0100

    polishing web app

---
+++ BoardGameCollection.Web/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BoardGameCollection.Web
+{
+    public static class Constants
+    {
+        public const string ApplicationName = "Board Game Collection";
+        public const string ApplicationNameAbbr = "BGC";
+    }
+}
